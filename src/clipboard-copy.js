73e74009b670825848896ab9068104507487531a
georapbox <georapbox@gmail.com>
2022-04-10 23:25:49 +0300

    Check if Declarative Shadow Root is present.

--- src/clipboard-copy.js
+++ src/clipboard-copy.js
@@ -10,13 +10,13 @@ class ClipboardCopy extends HTMLElement {
   constructor() {
     super();
 
-    this.attachShadow({ mode: 'open' });
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    if (!this.shadowRoot) {
+      this.attachShadow({ mode: 'open' });
+      this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
 
     this._buttonSlot = this.shadowRoot.querySelector('slot[name="button"]');
-    this.$button = this._buttonSlot.assignedNodes({ flatten: true }).find(el => {
-      return el.nodeType === 1 && (el.nodeName === 'BUTTON' || el.getAttribute('slot') === 'button');
-    });
+    this.$button = this._getButton();
 
     this._onClick = this._onClick.bind(this);
     this._onSlotChange = this._onSlotChange.bind(this);
@@ -27,7 +27,7 @@ class ClipboardCopy extends HTMLElement {
   }
 
   connectedCallback() {
-    this._buttonSlot.addEventListener('slotchange', this._onSlotChange);
+    this._buttonSlot && this._buttonSlot.addEventListener('slotchange', this._onSlotChange);
     this.$button && this.$button.addEventListener('click', this._onClick);
 
     this._upgradeProperty('value');
@@ -120,6 +120,16 @@ class ClipboardCopy extends HTMLElement {
     }
   }
 
+  _getButton() {
+    if (!this._buttonSlot) {
+      return null;
+    }
+
+    return this._buttonSlot.assignedNodes({ flatten: true }).find(el => {
+      return el.nodeType === 1 && (el.nodeName === 'BUTTON' || el.getAttribute('slot') === 'button');
+    });
+  }
+
   _onClick(evt) {
     evt.preventDefault();
 
@@ -137,9 +147,7 @@ class ClipboardCopy extends HTMLElement {
   _onSlotChange(evt) {
     if (evt.target && evt.target.name === 'button') {
       this.$button && this.$button.removeEventListener('click', this._onClick);
-      this.$button = this._buttonSlot.assignedNodes({ flatten: true }).find(el => {
-        return el.nodeType === 1 && (el.nodeName === 'BUTTON' || el.getAttribute('slot') === 'button');
-      });
+      this.$button = this._getButton();
 
       if (this.$button) {
         this.$button.addEventListener('click', this._onClick);
