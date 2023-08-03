6f32f29b3a80f0ce95fbfe7898ece51ebaad8cb8
georapbox <georapbox@gmail.com>
2023-08-03 22:15:26 +0300

    Fix component path

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,5 @@
 const isLocalhost = window.location.href.includes('127.0.0.1') || window.location.href.includes('localhost');
-const componentUrl = !isLocalhost ? '../../dist/clipboard-copy-defined.js' : '../lib/clipboard-copy-defined.js';
+const componentUrl = isLocalhost ? '../../dist/clipboard-copy-defined.js' : '../lib/clipboard-copy-defined.js';
 
 import(componentUrl).then(() => {
   document.addEventListener('clipboard-copy-success', evt => {
